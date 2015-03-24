1c0a5b7bb66a8552b6582b7e965d1ab5e8e5e55d
piersy <pierspowlesland@gmail.com>
2015-03-24 01:00:46 +0000

    Added the CookieConfigurationFunctionFactory

--- example/example-client/src/main/java/com/piersyp/dynasors/example/client/dynamic/CookieConfigurationFunctionFactory.java
+++ example/example-client/src/main/java/com/piersyp/dynasors/example/client/dynamic/CookieConfigurationFunctionFactory.java
@@ -1,4 +1,22 @@
 package com.piersyp.dynasors.example.client.dynamic;
 
-public class CookieConfigurationFunctionFactory {
+import com.sun.jersey.api.client.WebResource;
+
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.core.Cookie;
+import java.util.function.BiFunction;
+
+public class CookieConfigurationFunctionFactory implements BiFunction<CookieParam, Integer, BiFunction<Object[], WebResource.Builder, WebResource.Builder>>{
+
+    @Override
+    public BiFunction<Object[], WebResource.Builder, WebResource.Builder> apply(CookieParam cookieParam, Integer integer) {
+        return new BiFunction<Object[], WebResource.Builder, WebResource.Builder>() {
+            @Override
+            public WebResource.Builder apply(Object[] objects, WebResource.Builder builder) {
+                //TODO allow customisation of this to string approach
+                builder.cookie(new Cookie(cookieParam.value(), objects[integer].toString()));
+                return builder;
+            }
+        };
+    }
 }
---
+++ example/example-client/src/test/java/com/piersyp/dynasors/example/client/dynamic/CookieConfigurationFunctionFactoryUnitTest.java
@@ -0,0 +1,60 @@
+package com.piersyp.dynasors.example.client.dynamic;
+
+import com.piersyp.dynasors.example.client.AnnotationGenerator;
+import com.sun.jersey.api.client.WebResource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.core.Cookie;
+
+import java.lang.annotation.Annotation;
+import java.util.function.BiFunction;
+
+import static com.piersyp.dynasors.example.client.AnnotationGenerator.*;
+import static org.hamcrest.Matchers.any;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CookieConfigurationFunctionFactoryUnitTest {
+
+    public static final String COOKIE_VALUE = "cookieValue";
+    @Mock
+    private WebResource.Builder webResourceBuilderMock;
+    @Captor
+    private ArgumentCaptor<Cookie> cookieCaptor;
+
+    private Object[] params;
+    private CookieParam cookieParam;
+    private CookieConfigurationFunctionFactory cookieConfigurationFunctionFactory;
+
+    @Before
+    public void setUp() throws Exception {
+        when(webResourceBuilderMock.cookie(org.mockito.Matchers.any(Cookie.class))).thenReturn(webResourceBuilderMock);
+        params = new Object[1];
+        params[0] = COOKIE_VALUE;
+        cookieParam = (CookieParam) new AnnotationGenerator().getParameterAnnotationInstance(COOKIE_PARAM_CLASS);
+
+        cookieConfigurationFunctionFactory = new CookieConfigurationFunctionFactory();
+    }
+
+    @Test
+    public void whenCreateConfigurationFunction_thenFunctionBehavesAsExpected() throws Exception {
+        BiFunction<Object[], WebResource.Builder, WebResource.Builder> cookieConfigurationFunction = cookieConfigurationFunctionFactory.apply(cookieParam, 0);
+
+        cookieConfigurationFunction.apply(params, webResourceBuilderMock);
+
+        verify(webResourceBuilderMock).cookie(cookieCaptor.capture());
+        verifyNoMoreInteractions(webResourceBuilderMock);
+        assertThat(cookieCaptor.getValue(), equalTo(new Cookie(cookieParam.value(), COOKIE_VALUE)));
+    }
+
+
+}
